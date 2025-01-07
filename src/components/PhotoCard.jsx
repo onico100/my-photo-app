493b4a3604b8dbf9bb2340ce56a4cf6304090de1
onico100 <142163577+onico100@users.noreply.github.com>
2025-01-07 14:19:45 +0200

    stle

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,4 +1,6 @@
 "use client";
+import { CiEdit } from "react-icons/ci";
+import { MdOutlineDelete } from "react-icons/md";
 export default function PhotoCard({ photo, onEdit, onDelete }) {
   return (
     <div className="photo-card">
@@ -6,8 +8,12 @@ export default function PhotoCard({ photo, onEdit, onDelete }) {
       <h3>{photo.title}</h3>
       <p>ID: {photo.id}</p>
       <div className="butoon-conteiner">
-        <button onClick={onEdit}>Edit</button>
-        <button onClick={onDelete}>Delete</button>
+        <button onClick={onEdit}>
+          <CiEdit />
+        </button>
+        <button onClick={onDelete}>
+          <MdOutlineDelete />
+        </button>
       </div>
     </div>
   );
